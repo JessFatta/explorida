a7af19945f5d6b95243a8edf81eeb9e417a58dad
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 18:58:28 -0400

    Create basic structure of nav component

---
+++ src/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav-container'>
+      <h2>this is the nav</h2>
+    </div>
+  )
+}
+
+export default Nav
