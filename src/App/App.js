9a3048c615c28f2ebfaf20635fcfb973a6418bbe
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 18:39:55 -0400

    Create App components folder

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component{
+  constructor() {
+    super()
+    this.state = {
+      ideas: []
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1> heyyyyy</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
