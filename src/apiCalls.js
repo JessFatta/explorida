10952e214fe40dc0e7b8559afedfe20ab6694a6f
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 19:09:50 -0400

    Begin fetching data

---
+++ src/apiCalls.js
@@ -0,0 +1,21 @@
+const getAllFloridaParksData = () => {
+  return fetch('https://developer.nps.gov/api/v1/parks?stateCode=fl&api_key=ikJQlV6Nf6rZWouLdPDfsJ4IG0kFDzKuSgMrFvLw')
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+const getSingleParkData = (parkCode) => {
+  return fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=ikJQlV6Nf6rZWouLdPDfsJ4IG0kFDzKuSgMrFvLw`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+}
+
+const handleResponse = () => {
+  if(!response.ok) {
+    throw new Error('Something went wrong!')
+  } else {
+    return response.json()
+  }
+}
+
+export default getAllFloridaParksData
