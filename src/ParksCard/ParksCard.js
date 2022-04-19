a27f003c142e73fd5f95e0959558239760d43843
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 19:57:07 -0400

    Create parks card folder and components

---
+++ src/ParksCard/ParksCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ParksCard.css'
+
+const ParksCard = () => {
+  return (
+    <div className='parks-card'>
+      
+    </div>
+  )
+}
+
+export default ParksCard
