5a086433f4ab3a09a7ce4cca2d5e06d49fa6530e
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 20:58:06 -0400

    Get images to display

--- src/ParksCard/ParksCard.js
+++ src/ParksCard/ParksCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './ParksCard.css'
 
-const ParksCard = () => {
+const ParksCard = ({image, url, fullName}) => {
   return (
     <div className='parks-card'>
-      
+      <img src={image[1].url} alt={`${fullName}'s image`} className='parks-card-image' />
+      <p>{fullName}</p>
     </div>
   )
 }
