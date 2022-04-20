32c87e6f708a972cab152a65119b8743f901b631
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 22:27:57 -0400

    Add link

--- src/ParksCard/ParksCard.js
+++ src/ParksCard/ParksCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './ParksCard.css'
 
-const ParksCard = ({image, id, fullName}) => {
+const ParksCard = ({image, fullName}) => {
   return (
     <div className='parks-card'>
-      <Link to={`/${id}`} >
+      <Link to={`/${fullName}`} >
       <img src={image[1].url} alt={`${fullName}'s image`} className='parks-card-image' />
       <p>{fullName}</p>
       </Link>
