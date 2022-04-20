738c1425fbc0cefa965bc2a28a192599d1fe6e48
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 20:57:23 -0400

    Map over parks

--- src/AllParks/AllParks.js
+++ src/AllParks/AllParks.js
@@ -2,6 +2,26 @@ import React from 'react'
 import ParksCard from '../ParksCard/ParksCard'
 import './AllParks.css'
 
-const AllParks = () => {
+const AllParks = ({parks}) => {
+  const parkCards = parks.map(park => {
+    return (
+      <ParksCard 
+        image={park.images}
+        fullName={park.fullName}
+        key={park.id}
+        id={park.id}
+      />
+    )
+  })
 
+  return (
+    <div className='all-parks-wrapper'>
+      <h2>All Parks</h2>
+      <div className='all-parks-container'>
+        {parkCards}
+      </div>
+    </div>
+  )
 }
+
+export default AllParks
