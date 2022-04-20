b7f51e77c9a64a3053a2ff9b7897b8be171cd71d
Jessica Fatta <jessicafatta@gmail.com>
2022-04-19 22:28:19 -0400

    Add map to function to get park code

---
+++ src/SinglePark/SinglePark.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {getSingleParkData } from '../apiCalls'
+import './SinglePark.css'
+
+class SinglePark extends Component {
+  constructor() {
+    super()
+    this.state = {
+      singlePark: []
+    }
+  }
+
+  getParkCodes(parkCode) {
+    this.props.parkCode.map(code => getSingleParkData(code))
+  }
+
+  render() {
+    return (
+      <section className='single-park-page'>
+        <article className='single-park-details'>
+          <div className='single-park-text'>
+            {/* <p>{this.state.singlePark.fullName}</p> */}
+          </div>
+        </article>
+      </section>
+    )
+  }
+}
+
+export default SinglePark
