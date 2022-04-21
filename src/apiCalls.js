35c51f952093a35ee7d24fa6fac7feb6d9e0b0bc
Jessica Fatta <jessicafatta@gmail.com>
2022-04-21 18:41:04 -0400

    Add error handling to api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,11 @@
 const getAllFloridaParksData = () => {
   return fetch('https://developer.nps.gov/api/v1/parks?stateCode=fl&api_key=ikJQlV6Nf6rZWouLdPDfsJ4IG0kFDzKuSgMrFvLw')
-  .then(response => response.json())
-  //.then(data => console.log(data))
+  .then(response => handleResponse(response))
 }
 
 const getSingleParkData = (parkCode) => {
   return fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=ikJQlV6Nf6rZWouLdPDfsJ4IG0kFDzKuSgMrFvLw`)
-  .then(response => response.json())
-  //.then(data => console.log(data))
+  .then(response => handleResponse(response))
 }
 
 const handleResponse = (response) => {
