5ff93fe841f1b8323da43b874962d3ab8c546494
Jessica Fatta <jessicafatta@gmail.com>
2022-04-20 20:47:19 -0400

    Switch to link

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Nav.css'
 
 const Nav = () => {
   return (
     <div className='nav-container'>
-      <div className='title'>Explorida</div>
+      <NavLink to='/'>
+        <div className='title'>Explorida</div>
+      </NavLink>
     </div>
   )
 }
