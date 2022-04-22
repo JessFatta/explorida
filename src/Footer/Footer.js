8d34cf5fc8bea1ffa4a18fa3784b1258ae4e47fb
Jessica Fatta <jessicafatta@gmail.com>
2022-04-21 21:16:47 -0400

    Add footer components

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Footer.css"
+
+
+const Footer = () => {
+  return(
+    <footer>
+      <p className="footer-text">©2022 Explorida by Jess Fatta</p>
+      <p className="footer-text">|</p>
+      <p className="footer-text">Surrender to the Flo...</p>
+    </footer>
+
+  )
+}
+
+
+export default Footer
