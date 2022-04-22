7bd16fad87b9f31cabe3cd6c3649f42f8771cfa5
Jessica Fatta <jessicafatta@gmail.com>
2022-04-21 21:19:24 -0400

    End of day commit

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import FilterActivities from '../FilterActivities/FilterActivities'
 import './Nav.css'
 
-const Nav = () => {
+const Nav = ({FilterActivities}) => {
   return (
     <div className='nav-container'>
       <NavLink to='/'>
         <div className='title'>Explorida</div>
       </NavLink>
+      {/* <FilterActivities FilterActivities={FilterActivities} /> */}
     </div>
   )
 }
