b8508099379ef42ee77d651455e66c331f8a1830
Jessica Fatta <jessicafatta@gmail.com>
2022-04-22 15:42:20 -0400

    Create components for filitered parks

---
+++ src/FilteredParks/FilteredParks.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ParksCard from '../ParksCard/ParksCard'
+
+
+const FilteredParks = ({filteredParks, designation}) => {
+  const filteredParksCards = filteredParks.map(park => {
+    return (
+      <ParksCard 
+        image={park.images}
+        fullName={park.fullName}
+        key={park.id}
+        id={park.parkCode}
+        parkCode={park.parkCode}
+      />
+    )
+  })
+
+
+
+  return (
+    <div className='all-parks-wrapper'>
+      <h2 className='explore-all-parks'>{designation}</h2>
+      <div className='all-parks-container'>
+        {filteredParksCards}
+      </div>
+    </div>
+  )
+}
+
+export default FilteredParks
